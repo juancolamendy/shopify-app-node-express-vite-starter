4d3a7e59484809f40a592144ec5b9095e2d947be
JC Olamendy <juanc.olamendy@gmail.com>
2022-06-29 17:46:59 -0400

    add products page

--- frontend/pages/products.jsx
+++ frontend/pages/products.jsx
@@ -1,5 +1,46 @@
+import { useState } from 'react';
+
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
 const Products = () => {
-  return (<h1>Products</h1>)
+  const [open, setOpen] = useState(false);
+
+  const handleSelection = (resources) => {
+    setOpen(false);
+    console.log(resources);
+  };
+
+  return (
+    <Page>
+        <TitleBar primaryAction={{
+          content: 'Select products',
+          onAction: () => setOpen(true),
+        }} />
+
+        <ResourcePicker
+          resourceType="Product"
+          showVariants={false}
+          open={open}
+          onSelection={handleSelection}
+          onCancel={() => setOpen(false)}
+        />
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: 'Select products',
+              onAction: () => setOpen(true),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>        
+    </Page>
+  );
 }
 
 export default Products;
